2b6e5287baf203e73fe2fc76d0cc1e75db865b56
Cauã Ribeiro <cauarbeiro30@gmail.com>
2026-07-23 00:24:10 -0300

    fix: adicionando validacao ao deletar produto com pedidos vinculados

--- backend/src/main/java/CaseJava/ApiCliente/repository/CidadeRepository.java
+++
@@ -1,8 +0,0 @@
-package CaseJava.ApiCliente.repository;
-
-
-import CaseJava.ApiCliente.entity.Cidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CidadeRepository extends JpaRepository<Cidade, Long> {
-}
